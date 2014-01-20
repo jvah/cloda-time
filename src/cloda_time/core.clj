634282dd70a5819b7d397bc3e67909babd4227a6
Juho Vähä-Herttua <juho.vaha-herttua@futurice.com>
2014-01-20 11:34:53 +0200

    Initial commit

---
+++ src/cloda_time/core.clj
@@ -0,0 +1,6 @@
+(ns cloda-time.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/cloda_time/core_test.clj
@@ -0,0 +1,7 @@
+(ns cloda-time.core-test
+  (:require [clojure.test :refer :all]
+            [cloda-time.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
